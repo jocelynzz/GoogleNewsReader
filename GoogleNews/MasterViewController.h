931a91ca162981bf0fa2c0df0e9e3b6d5b66ab63
ZHAO ZHANG <jocelynz@uchicago.edu>
2015-02-13 16:08:23 -0600

    Initial Commit

---
+++ GoogleNews/MasterViewController.h
@@ -0,0 +1,19 @@
+//
+//  MasterViewController.h
+//  GoogleNews
+//
+//  Created by Jocelyn on 2/13/15.
+//  Copyright (c) 2015 Jocelyn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+
+@end
+
